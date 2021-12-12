9cde7c480abaaff12b9ab18002065e4bc5160982
TheCaptainCraken <thecaptaincraken@gmail.com>
2021-12-12 12:44:53 +0100

    added pragma once and comments

--- stacks.h
+++ stacks.h
@@ -1,6 +1,11 @@
+#pragma once
 #include <stdbool.h>
 
+/* allocates space for the stack in the heap */
 int* CreateStack(const int max_size, int* current_size);
+/* Pushes a value in the stacks (returns true if it was succcesful) */
 bool Push(int* stack, const int max_size, int* current_size, int element);
+/* Pops the last value inserted in the stack */
 int Pop(int* stack, int* current_size);
+/* prints all the stack in the std output */
 void PrintStack(int* stack, int current_size);
